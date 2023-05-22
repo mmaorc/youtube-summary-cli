b71c0b12a96c5afc7c87868335cb40df2fbe15a3
Maor Cohen <me@maorcohen.net>
2023-05-22 13:02:41 +0300

    Add description to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="youtube-summary",
     version="0.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[line.strip() for line in open("requirements.txt")],
     entry_points={
