bfa92b1188dc460d6f84a24584c7903e3a670ef3
Maor Cohen <me@maorcohen.net>
2023-05-22 13:09:35 +0300

    Update version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="youtube-summary",
-    version="0.1",
+    version="0.1.1",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
