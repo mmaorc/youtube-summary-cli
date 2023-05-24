9ae11f36cd9a4261b1f04629ce4b8f46082bac83
Maor Cohen <me@maorcohen.net>
2023-05-24 10:44:59 +0300

    Bump version to 0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="youtube-summary",
-    version="0.3",
+    version="0.4",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
